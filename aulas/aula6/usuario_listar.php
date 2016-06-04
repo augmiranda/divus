f79a1d15e5c7ac21215708fd2b15b9a27edfb9d0
Railton Nepomuceno <railton@gmail.com>
2016-06-04 09:10:01 -0400

    aula6

--- aulas/aula6/usuario_listar.php
+++ aulas/aula6/usuario_listar.php
@@ -62,6 +62,7 @@ if(isset($_SESSION['alerts'])){
           
           <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    
       <ul class="nav navbar-nav">
          <li><a href="usuario_listar.php">Usuário</a></li>
          <li><a href="setor_listar.php">Setor</a></li>
